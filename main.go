997fd0194d95c693e8222a5f7fce301fe04ae76f
a11yster <menonr96@gmail.com@users.noreply.github.com>
2025-04-08 12:53:34 +0530

    feat: add switch case for cli args

--- main.go
+++ main.go
@@ -12,4 +12,12 @@ func main() {
 		fmt.Println("Usage: gRUN <command>")
 		os.Exit(1)
 	}
+
+	switch os.Args[1] {
+	case "run":
+		fmt.Println("Running the graph...")
+	default:
+		fmt.Println("Unknown command:", os.Args[1])
+		os.Exit(1)
+	}
 }
